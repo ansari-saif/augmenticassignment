7ceae158feab26736341fd78092d66107f78ffa3
Rohit Singh <rhtsing@gmail.com>
2022-04-21 01:14:48 +0530

    vendor schema

--- src/db/schema/vendor.ts
+++ src/db/schema/vendor.ts
@@ -5,11 +5,47 @@ import { Vendor } from "../../models";
 
 interface IVendor extends Document {
   name: string;
-  email: string;
-  phone: string;
+  firstName: string;
+  lastName: string;
   company: string;
-  address: string;
+  email: string;
+  phone: number;
+  mobile: number;
+  website: string;
   vendorType: string;
+  otherDetails: {
+    pan: string;
+    gst: string;
+    openingBalance: number;
+    paymentTerms: string;
+    tds: string;
+    currency: string;  
+  };
+  billAddress: {
+    attention: string;
+    address: string;
+    city: string;
+    state: string;
+    pincode: number;
+    country: string;
+    phone: number;  
+  };
+  shipAddress: {
+    attention: string;
+    address: string;
+    city: string;
+    state: string;
+    pincode: number;
+    country: string;
+    phone: number;  
+  };
+  personContact: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: number;
+    mobile: number;
+  }[],
   projectList: Types.ObjectId[];
   vendorCredits: number[];
   expenses: number[];
@@ -20,11 +56,47 @@ interface IVendor extends Document {
 const vendorSchema = new Schema<IVendor>(
   {
     name: String,
-    email: String,
-    phone: String,
+    firstName: String,
+    lastName: String,
     company: String,
-    address: String,
+    email: String,
+    phone: Number,
+    mobile: Number,
+    website: String,
     vendorType: String,
+    otherDetails: {
+      pan: String,
+      gst: String,
+      openingBalance: Number,
+      paymentTerms: String,
+      tds: String,
+      currency: String
+    },
+    billAddress: {
+      attention: String,
+      address: String,
+      city: String,
+      state: String,
+      pincode: Number,
+      country: String,
+      phone: Number
+    },
+    shipAddress: {
+      attention: String,
+      address: String,
+      city: String,
+      state: String,
+      pincode: Number,
+      country: String,
+      phone: Number
+    },
+    personContact: [{
+      firstName: String,
+      lastName: String,
+      email: String,
+      phone: Number,
+      mobile: Number
+    }],
     vendorCredits: [{ type: Number, ref: "VendorCredit" }],
     projectList: [{ type: Schema.Types.ObjectId, ref: "Project" }],
     expenses: [{ type: Number, ref: "Expense" }],
