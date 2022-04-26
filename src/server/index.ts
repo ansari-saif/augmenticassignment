257c4e3b630f6cfd1d5d3a7676b4af5e84b5a0ae
m-r-mallick <mrmblogger@gmail.com>
2022-04-27 01:03:13 +0530

    changes to CORS config

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,12 +5,24 @@ import fileUpload from "express-fileupload";
 
 const app = require("express")();
 
+const whitelist = ["http://127.0.0.1:8001", "https://hrm-xi.vercel.app"];
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(express.json({ limit: "50mb" }));
 app.use(
-  cors({
-    origin: "http://127.0.0.1:8001",
-    methods: "GET,POST,PUT,PATCH,DELETE",
-  })
+  // cors({
+  //   origin: "http://127.0.0.1:8001",
+  //   methods: "GET,POST,PUT,PATCH,DELETE",
+  // })
+  cors(corsOptions)
 );
 app.use(fileUpload());
 
