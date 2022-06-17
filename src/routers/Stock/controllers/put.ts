0b2a0009f402f77fe6505309eb5276569ffdf59e
Rohit Singh <rhtsing@gmail.com>
2022-06-17 18:46:56 +0530

    stock api impro

--- src/routers/Stock/controllers/put.ts
+++ src/routers/Stock/controllers/put.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { SplitStock } from "../../../models/splitStock";
 import { Stock } from "../../../models/stock";
 
 export default async function controllerPut(req: Request, res: Response) {
@@ -10,5 +11,17 @@ export default async function controllerPut(req: Request, res: Response) {
 
   } catch (err) {
     res.status(500).json({ msg : "Stock was not updated" })
+  }
+} 
+
+export async function SplitStkPut(req: Request, res: Response) {
+  try {
+    
+    const { id } = req.params;
+    const splitStock = await SplitStock.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(splitStock);
+
+  } catch (err) {
+    res.status(500).json({ msg : "Split Stock was not updated" })
   }
 } 
