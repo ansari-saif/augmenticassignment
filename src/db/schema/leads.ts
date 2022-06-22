d3a76888cb0e684f86b583bb739e8a6d57006cea
nishchal singh <singhnishchal27@gmail.com>
2022-06-22 17:48:34 +0530

    leads error resolved for now

--- src/db/schema/leads.ts
+++ src/db/schema/leads.ts
@@ -43,6 +43,7 @@ interface ILead extends Document {
   assignType: string;
   currentAssigned: number;
   activities: ILeadActivity[];
+  interest: [string]; //added this to remove error from.
   // not used
 }
 
