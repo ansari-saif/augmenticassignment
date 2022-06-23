23aee8ab9777eddbb25e586622258585b362f1f9
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2022-06-23 15:53:54 +0530

    lead profile complete

--- src/routers/lead/controllers/get.ts
+++ src/routers/lead/controllers/get.ts
@@ -7,6 +7,11 @@ export default async function controllerGet(req: Request, res: Response) {
   const { id } = req.params;
   if (id) {
     const lead = await Lead.findById(id)
+      .populate('createdBy')
+      .populate('assignedTo')
+      .populate('currentAssigned')
+      .populate('project')
+      .populate('status')
     if (!lead) {
       return res.status(404).send({ message: "Lead not found" });
     } else {
@@ -19,6 +24,7 @@ export default async function controllerGet(req: Request, res: Response) {
       .populate('currentAssigned')
       .populate('project')
       .populate('status')
+    console.log(leads);
     res.status(200).send(leads);
   }
 }
