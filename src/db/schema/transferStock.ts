9d798eee11b1efedaf221ed4d1c689b98e937d0c
Rohit Singh <rhtsing@gmail.com>
2022-07-11 15:55:29 +0530

    schema changes to project and stock

--- src/db/schema/transferStock.ts
+++ src/db/schema/transferStock.ts
@@ -4,7 +4,8 @@ import { Schema, Document, Types } from "mongoose";
 interface ITransferStock extends Document {
   date: Date;
   stockId: Types.ObjectId;
-  splitStockId: Types.ObjectId;
+  unit: string;
+  // splitStockId: Types.ObjectId;
   transferQuantity: Number;
   projectFrom: Types.ObjectId;
   projectTo: Types.ObjectId;
@@ -13,8 +14,9 @@ interface ITransferStock extends Document {
 const transferStockSchema = new Schema<ITransferStock>({
   date: Date,
   stockId: { type: Schema.Types.ObjectId, ref: "Stock" },
-  splitStockId: { type: Schema.Types.ObjectId, ref: "SplitStock" },
+  // splitStockId: { type: Schema.Types.ObjectId, ref: "SplitStock" },
   transferQuantity: Number,
+  unit: { type: String, default: "pcs." },
   projectFrom: { type: Schema.Types.ObjectId, ref: "Project" },
   projectTo: { type: Schema.Types.ObjectId, ref: "Project" },
 });
