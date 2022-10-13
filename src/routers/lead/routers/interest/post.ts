6bdde78c46df6b28b5dd4343b270d17dedf7e340
ROHIT SINGH <55020650+RhtWeb@users.noreply.github.com>
2022-10-13 10:21:18 +0530

    Update post.ts

--- src/routers/lead/routers/interest/post.ts
+++ src/routers/lead/routers/interest/post.ts
@@ -36,10 +36,10 @@ export default async function controllerPost(req: Request, res: Response) {
       message: "Plot not found",
     });
   }
-  leadPlot.leadsInfo.push({
-    leadType: leadInterst.leadType,
-    lead: lead._id,
-  });
+//   leadPlot.leadsInfo.push({
+//     leadType: leadInterst.leadType,
+//     lead: lead._id,
+//   });
   await lead.save();
   await project.save();
   return res.status(200).json({
