cdd1bd5caf793731083c7e7c74d948db334d19f1
unknown <kumarritik588@gmail.com>
2022-10-17 13:18:11 +0530

    change_empolyee_lead

--- src/routers/lead/controllers/get.ts
+++ src/routers/lead/controllers/get.ts
@@ -29,17 +29,22 @@ export default async function controllerGet(req: Request, res: Response) {
 }
 
 export async function controllerGetByEmployee(req: Request, res: Response) {
+  
   const { id } = req.params;
+  console.log(id,"ididididididid");
   if (id) {
-    const leads: any = await Lead.find({ currentAssigned: id })
+    // const leads: any = await Lead.find({ currentAssigned: id })
+    const leads: any = await Lead.find({ createdBy : id })
       .populate('createdBy')
       .populate('assignedTo')
       .populate('currentAssigned')
       .populate('project')
       .populate('status');
     if (!leads) {
+      console.log(leads,"Lead not found");
       return res.status(404).send({ message: "Lead not found" });
     } else {
+      console.log(leads,"Lead  found");
       return res.status(200).send(leads);
     }
   } else {
